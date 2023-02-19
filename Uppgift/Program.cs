8518e01dd7ee60970f751a6ea2c2c1f600f2233a
Gunnnnnerr <117036398+Gunnnnnerr@users.noreply.github.com>
2023-02-19 23:57:54 +0100

    Add project files.

---
+++ Uppgift/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace uppgift_6._2
+{
+    class Program
+    {
+        static int Störst(int tal1, int tal2)
+        {
+            if (tal1 < tal2)
+                return tal2;
+            else
+                return tal1;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in två tal");
+            Console.Write("Tal 1: "); int tal1 = int.Parse(Console.ReadLine());
+            Console.Write("Tal 2: "); int tal2 = int.Parse(Console.ReadLine());
+
+            int störst = Störst(tal1, tal2);
+
+            Console.WriteLine($"Det sörsta talet utav de två är {störst}");
+        }
+    }
+}
